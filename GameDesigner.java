045165054f7510aea6890264ca5576743074c701
Yinghui Huang <yh2639@columbia.edu>
2014-04-16 02:11:45 -0400

    testcode

--- GameDesigner.java
+++ GameDesigner.java
@@ -1,4 +1,6 @@
 package edu.columbia.PLT.BGD;
+import java.lang.String;
+import java.util.*;
 public class GameDesigner{
 enum pieceType{STONE};
 static int [] pieceNum = {0};
@@ -7,24 +9,36 @@ static int boardCol = 3;
 static int playerNum = 2;
 public static boolean add_res (int[] position)
 {
+int[] pos={4, 4};
+
+String[] a={"1", "2", "3"};
+
 return Functions.isEmpty(position);
 }
 public static boolean win_res (int[] position)
 {
+int a=4;
+
 if(Functions.numberInRow(position)>=3)
 {
 return true;
 }
-else
+else if(a>3){
+while(a<=4)
 {
 return false;
 }
 }
-public static boolean move_res(int[] par1,int[] par2)
+else
 {
 return true;
 }
-public static boolean remove_res(int[] par1)
+}
+public static boolean move_res(int[] par0,int[] par1)
+{
+return true;
+}
+public static boolean remove_res(int[] par0)
 {
 return true;
 }
