42aefb0a39fbf45698191d6c72133a9d7ac60631
Xingyu <xingyu1988@gmail.com>
2014-04-30 09:57:45 -0400

    Update Drawing.java

--- PLTImplementation/src/Drawing.java
+++ PLTImplementation/src/Drawing.java
@@ -2,33 +2,52 @@ import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 /**
  * Defination of drawing, to draw the graph of the chess board just in time.
- * 
  * @author Presenthuang
- * 
+ *
  */
 public class Drawing {
-	public static void drawBoard(ArrayList<Player> pl, String[] icons) {
+	public static void drawBoard(BoardGUI board, ArrayList<Player> pl, String[] icons)
+	{
 		Slot[][] slots = Board.boardslots;
 		int numOfRows = Board.row();
 		int numOfColumns = Board.col();
-
-		for (int i = 0; i < numOfRows; i++) {
-			for (int j = 0; j < numOfColumns; j++) {
-				try {
-					// if(slots[i][j].Player().getId() == 0)
-					// System.out.print("o ");
-					// else
-					// System.out.print("x ");
-					System.out.print(icons[slots[i][j].Player().getId()] + " ");
-				} catch (NullPointerException e) {
-					System.out.print(". ");
+		
+		for(int i = 0; i < numOfRows; i++)
+		{
+			for(int j = 0; j < numOfColumns; j++)
+			{
+				try{
+					//System.out.print(icons[slots[i][j].Player().getId()] + " ");
+					add(board, i+""+j, icons[slots[i][j].Player().getId()]);
 				}
-
-			}
-			System.out.println("");
+				catch(NullPointerException e)
+				{
+					
+				}		
+			}	
 		}
+	}
+
+	
+	public static BoardGUI drawInitialBoard()
+	{
+		try{
+			Image whiteBlock = ImageIO.read(new File("/../wood.jpg"));
+			BoardGUI board = new BoardGUI(whiteBlock);
+			return board;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+		
+
+
+
 	}
 
 	public static void add(BoardGUI board, String position, String iconName)
