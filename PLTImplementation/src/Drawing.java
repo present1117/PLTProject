aecc25fe8e289e38900693ecd35df1161170eda7
Xingyu <xingyu1988@gmail.com>
2014-05-09 10:25:01 -0400

    Update Drawing.java

--- PLTImplementation/src/Drawing.java
+++ PLTImplementation/src/Drawing.java
@@ -6,15 +6,12 @@ import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-
 /**
  * Defination of drawing, to draw the graph of the chess board just in time.
- * 
  * @author Presenthuang
- * 
+ *
  */
 public class Drawing {
-<<<<<<< HEAD
 	public static void drawBoard(BoardGUI board, ArrayList<Player> pl, HashMap[] iconPool, String pieceType, int posx, int posy)
 	{
 		Slot[][] slots = Board.boardslots;
@@ -45,27 +42,6 @@ public class Drawing {
 	public static BoardGUI drawInitialBoard()
 	{
 		try{
-=======
-	public static void drawBoard(BoardGUI board, ArrayList<Player> pl,
-			String[] icons) {
-		Slot[][] slots = Board.getBoardSlots();
-		int numOfRows = Board.getRow();
-		int numOfColumns = Board.getCol();
-
-		for (int i = 0; i < numOfRows; i++) {
-			for (int j = 0; j < numOfColumns; j++) {
-				try {
-					add(board, i + "" + j, icons[slots[i][j].Player().getId()]);
-				} catch (NullPointerException e) {
-
-				}
-			}
-		}
-	}
-
-	public static BoardGUI drawInitialBoard() {
-		try {
->>>>>>> FETCH_HEAD
 			Image whiteBlock = ImageIO.read(new File("wood.jpg"));
 			BoardGUI board = new BoardGUI(whiteBlock);
 			return board;
@@ -73,11 +49,15 @@ public class Drawing {
 			ex.printStackTrace();
 			return null;
 		}
+		
+
+
 
 	}
 
-	public static void add(BoardGUI board, String position, String iconName) {
-		// add pieces to board
+	public static void add(BoardGUI board, String position, String iconName)
+	{
+		//add pieces to board
 		board.addPiece(new ImageIcon(iconName), position);
 
 	}
