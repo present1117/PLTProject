976a57f97607660672a8181e40f363691a4c62d7
Ke Wu <wk09@me.com>
2014-05-10 21:58:26 -0400

    abc

--- PLTImplementation/src/GameDesigner.java
+++ PLTImplementation/src/GameDesigner.java
@@ -2,50 +2,39 @@ import java.lang.*;
 import java.util.*;
 
 public class GameDesigner {
-	static String[] pieceType = { "RED", "YELLOW", "GREEN", "BLUE"};
-	static int[] pieceNum = { 0, 0, 0, 0 };
-	static int boardRow = 8;
-	static int boardCol = 8;
+	static String[] pieceType = { "STONE" };
+	static int[] pieceNum = { 0 };
+	static int boardRow = 3;
+	static int boardCol = 3;
 	static int playerNum = 1;
 
-	public static boolean add_res(String pieceType, Pos position) {
-		Object PIECE_TYPE = pieceType;
+	static String[] initPieces = { "STONE", "STONE", "STONE", "STONE", "STONE",
+			"STONE", "STONE", "STONE", "STONE" };
+	static int[] initOwner = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	static int[][] initPos = { { 0, 0 }, { 0, 1 }, { 0, 2 }, { 1, 0 },
+			{ 1, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 }, { 2, 2 } };
 
-		if (PIECE_TYPE == "RED" || PIECE_TYPE == "YELLOW") {
-			return Functions.isEmpty(position);
-		}
-		
-		if (PIECE_TYPE == "GREEN") {
-			return Functions.isEmpty(position)
-					&& Functions.getPieceType(Functions.getPiece(position.x(),position.y() - 1)) == "RED"
-					&& Functions.getPieceType(Functions.getPiece(position.x(),position.y() + 1)) == "RED"
-					&& Functions.getPieceType(Functions.getPiece(position.x() - 1, position.y())) == "RED"
-					&& Functions.getPieceType(Functions.getPiece(position.x() + 1, position.y())) == "RED";
-		}
-
-		if (PIECE_TYPE == "BLUE") {
-			return Functions.isEmpty(position)
-					&& Functions.getPieceType(Functions.getPiece(position.x(),position.y() - 1)) == "RED"
-					&& Functions.getPieceType(Functions.getPiece(position.x() - 1, position.y())) == "RED"
-					&& Functions.getPieceType(Functions.getPiece(position.x(),position.y() + 1)) == "YELLOW"
-					&& Functions.getPieceType(Functions.getPiece(position.x() + 1, position.y())) == "YELLOW";
-		}
-		return false;
-	}
-
-	public static boolean win_res(Pos position) {
-		if (Functions.pieceCount("GREEN") + Functions.pieceCount("BLUE") == 15) {
+	public static boolean remove_res(Pos pos) {
+		if (Functions.getPiece(pos) != null) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public static boolean move_res(Pos par0, Pos par1) {
+	public static boolean win_res(Pos position) {
+		if (Functions.pieceCount("STONE") == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean add_res(String par0, Pos par1) {
 		return true;
 	}
 
-	public static boolean remove_res(Pos par0) {
+	public static boolean move_res(Pos par0, Pos par1) {
 		return true;
 	}
 }
